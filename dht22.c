299c6defc5e603a80fa7f61ee95df2e7a478662c
Guillaume Lavigne <guillaume.lavigne@free.fr>
2018-10-11 10:56:50 +0200

    First release

--- dht22.c
+++ dht22.c
@@ -19,67 +19,43 @@
 #include <linux/interrupt.h>
 #include <linux/gpio.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Guillaume Lavigne");
-MODULE_DESCRIPTION("DHT22 LKM");
-MODULE_VERSION("1.0");
-
-int err;
-static unsigned gpio = 15; // (1-1) * 32 + 15
-static unsigned int irq;
-static uint16_t temp, hum, var;
-static uint8_t chksum;
-
+#define GPIO 15 // (1-1) * 32 + 15
 
 typedef struct{
-	uint16_t hum;
-	uint16_t temp;
-	uint8_t chksum;
-	
+	unsigned int chksum : 8;
+	int temp : 16;
+	unsigned int hum : 16;
 } __attribute__((packed,aligned(1))) values;
 
-static values *val;
+static unsigned int irq;
+static uint16_t var;
+static uint8_t chksum;
 
-/**
- * Timing
-*/
+static values *val = NULL;
 static int Tbe = 1000;
-
 static struct kobject *dht22_kobject;
-
 struct timeval now;
-
 static uint32_t count = 0;
 static int time1, time2;
-
 static uint64_t raw_data=0;
 
 DECLARE_COMPLETION(cpl);
 
 /**
- * IRQ Handler 
+ * @brief IRQ Handler 
 */
 static irq_handler_t dht22_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
 	count++;
 	do_gettimeofday(&now);
 	if (count < 2);
 	// End of Frame
-	if (count==42){
+	if (count>=42){
 		time2=now.tv_usec;
 		if (time2-time1 > 100)
-			val->chksum = (val->chksum << 1) | 1;
+			raw_data = (raw_data << 1) | 1;
 		else
-			val->chksum = (val->chksum << 1) | 0;
+			raw_data = (raw_data << 1) | 0;
 		complete(&cpl);
-	}
-	// Checksum
-	if(count>=35){
-		time2=now.tv_usec;
-		if (time2-time1 > 100)
-			val->chksum = (val->chksum << 1) | 1;
-		else
-			val->chksum = (val->chksum << 1) | 0;
-		time1=time2;		
 	}
 	// Start of frame
 	else if(count==2)
@@ -96,50 +72,57 @@ static irq_handler_t dht22_irq_handler(unsigned int irq, void *dev_id, struct pt
 	return (irq_handler_t) IRQ_HANDLED;
 }
 
-static void data_management(void){
+/**
+ * @brief Data processing
+*/
+static int data_management(void){
+	
 	val = (values*) &raw_data;
 	val->hum /=10;
 	val->temp /=10;
 	chksum = (raw_data & 0xff) + (raw_data & 0xff00) + (raw_data & 0xff0000) + (raw_data & 0xff000000);
-	printk(KERN_INFO "CHK sent : %d, CHK computed : %d, raw_data : %d, temp = %d\n", val->chksum, chksum, raw_data, val->temp);
+	return (chksum != val->chksum);
 }
 
 /**
- * DHT22 init frame
+ * @brief DHT22 init frame
 */
-static void dht22_initialize(void){
+static int dht22_initialize(void){
+	int err;
 	count=0;
-	gpio_direction_output(gpio,1);
-	gpio_set_value(gpio,0);
+	gpio_direction_output(GPIO,1);
+	gpio_set_value(GPIO,0);
 	udelay(Tbe);
 	enable_irq(irq);
-	gpio_set_value(gpio,1);
-	gpio_direction_input(gpio);
+	gpio_set_value(GPIO,1);
+	gpio_direction_input(GPIO);
 
-	wait_for_completion_interruptible(&cpl);
+	wait_for_completion(&cpl);
 	disable_irq(irq);
-	gpio_direction_output(gpio,0);
-	data_management();
+	gpio_direction_output(GPIO,0);
+	err = data_management();
+	return err;
 }
 
 /**
- * Called on cat /sys/kernel/dht22_kobject/{temp,hum}
+ * @brief Called on cat /sys/kernel/dht22_kobject/{temp,hum}
 */
 static ssize_t b_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-	dht22_initialize();
+	while (dht22_initialize());
+	
 	if (strcmp(attr->attr.name, "temp") == 0){
-		var = temp;
+		var = val->temp;
 	}
 
 	else if(strcmp(attr->attr.name, "hum") == 0){
-		var = hum;
+		var = val->hum;
 	}
 
 	else{
 		printk(KERN_WARNING "Invalid attr");
-		gpio_set_value(gpio,1);
-		gpio_unexport(gpio);
-		gpio_free(gpio);
+		gpio_set_value(GPIO,1);
+		gpio_unexport(GPIO);
+		gpio_free(GPIO);
 		kobject_put(dht22_kobject);
 		return -EINVAL;
 	}
@@ -169,10 +152,11 @@ static struct attribute_group attr_group = {
 
 
 /**
- * Initialize LKM
+ * @brief Initialize LKM
 */
 static int __init dht22_init(void){
-	printk(KERN_INFO "Initializing DHT22 module\n");
+	int err;
+	printk(KERN_DEBUG "Initializing DHT22 module\n");
 	dht22_kobject = kobject_create_and_add("dht22_kobj",kernel_kobj);
 	if (!dht22_kobject){
 		printk(KERN_ERR "Cannot create kobject\n");
@@ -186,17 +170,17 @@ static int __init dht22_init(void){
 		return -ENOMEM;
 	}
 
-	if (!gpio_is_valid(gpio)){
+	if (!gpio_is_valid(GPIO)){
 		printk(KERN_ERR "Invalid GPIO number\n");
 		return -ENODEV;
 	}
 
-	gpio_request(gpio,"sysfs");
-	gpio_export(gpio,true);
-	gpio_direction_output(gpio,1);
+	gpio_request(GPIO,"sysfs");
+	gpio_export(GPIO,true);
+	gpio_direction_output(GPIO,1);
 
 	init_completion(&cpl);
-	irq = gpio_to_irq(gpio);
+	irq = gpio_to_irq(GPIO);
 	
 	err = request_irq(irq,
 			(irq_handler_t) dht22_irq_handler,
@@ -204,22 +188,25 @@ static int __init dht22_init(void){
 			"dht22_gpio_handler",
 			NULL);
 	disable_irq(irq);
-	val =vmalloc(sizeof(values));
 	return err;
 }
 
 /**
- * Exitting LKM
+ * @brief Exitting LKM
 */
 static void __exit dht22_exit(void){
-	gpio_unexport(gpio);
-	gpio_free(gpio);
+	gpio_unexport(GPIO);
+	gpio_free(GPIO);
 	kobject_put(dht22_kobject);
 	free_irq(irq,NULL);
-	vfree(val);
-	printk(KERN_INFO "Removing DHT22 module\n");
+	printk(KERN_DEBUG "Removing DHT22 module\n");
 }
 
 module_init(dht22_init);
 module_exit(dht22_exit);
 
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Guillaume Lavigne");
+MODULE_DESCRIPTION("DHT22 LKM");
+MODULE_VERSION("1.0");
