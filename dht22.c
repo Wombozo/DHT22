4cad3099560a675631194b4ba2e27d664131edf1
Guillaume Lavigne <guillaume.lavigne@free.fr>
2018-10-11 10:00:17 +0200

    [Start to handle chksum]

--- dht22.c
+++ dht22.c
@@ -14,7 +14,6 @@
 #include <linux/delay.h>
 #include <linux/completion.h>
 #include <linux/timekeeping.h>
-#include <linux/kthread.h>
 #include <linux/sched.h>
 
 #include <linux/interrupt.h>
@@ -27,56 +26,35 @@ MODULE_VERSION("1.0");
 
 int err;
 static unsigned gpio = 15; // (1-1) * 32 + 15
-//static int temp, hum;
+static unsigned int irq;
+static uint16_t temp, hum, var;
+static uint8_t chksum;
+
+
+typedef struct{
+	uint16_t hum;
+	uint16_t temp;
+	uint8_t chksum;
+	
+} __attribute__((packed,aligned(1))) values;
+
+static values *val;
 
 /**
  * Timing
 */
 static int Tbe = 1000;
-//static int Tgo = 30;
-//static int Trel = 80;
-//static int Treh = 80;
-//static int Tlow = 50;
-//static int Tho = 26;
-//static int Thi = 70;
-//static int Ten = 50;
 
 static struct kobject *dht22_kobject;
 
 struct timeval now;
 
-struct tasklet_struct dht22_tasklet;
-
-static int count = 0;
+static uint32_t count = 0;
 static int time1, time2;
 
-static char bit_buf[32];
-static int index=0;
+static uint64_t raw_data=0;
 
 DECLARE_COMPLETION(cpl);
-
-static void dht22_bhalf(unsigned long ctx);
-
-/**
- * Bottom half function (multi-threaded)
-*/
-static void dht22_bhalf(unsigned long time){
-	if (count < 5);
-	else if(count>=10)
-		complete(&cpl);
-	else if(count%2==1){
-		time1=time;
-	}
-	else{
-		time2=time;
-		if (time2-time1 > 50)
-			bit_buf[index]=1;
-		else
-			bit_buf[index]=0;
-		index++;
-	}
-	
-}
 
 /**
  * IRQ Handler 
@@ -84,34 +62,76 @@ static void dht22_bhalf(unsigned long time){
 static irq_handler_t dht22_irq_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
 	count++;
 	do_gettimeofday(&now);
-	tasklet_init(&dht22_tasklet, dht22_bhalf,(unsigned long)(now.tv_usec));
-	tasklet_schedule(&dht22_tasklet);
+	if (count < 2);
+	// End of Frame
+	if (count==42){
+		time2=now.tv_usec;
+		if (time2-time1 > 100)
+			val->chksum = (val->chksum << 1) | 1;
+		else
+			val->chksum = (val->chksum << 1) | 0;
+		complete(&cpl);
+	}
+	// Checksum
+	if(count>=35){
+		time2=now.tv_usec;
+		if (time2-time1 > 100)
+			val->chksum = (val->chksum << 1) | 1;
+		else
+			val->chksum = (val->chksum << 1) | 0;
+		time1=time2;		
+	}
+	// Start of frame
+	else if(count==2)
+		time1=now.tv_usec;
+	// Data
+	else{
+		time2=now.tv_usec;
+		if (time2-time1 > 100)
+			raw_data= raw_data << 1 | 1;
+		else
+			raw_data= raw_data << 1 | 0;
+		time1=time2;
+	}
 	return (irq_handler_t) IRQ_HANDLED;
+}
+
+static void data_management(void){
+	val = (values*) &raw_data;
+	val->hum /=10;
+	val->temp /=10;
+	chksum = (raw_data & 0xff) + (raw_data & 0xff00) + (raw_data & 0xff0000) + (raw_data & 0xff000000);
+	printk(KERN_INFO "CHK sent : %d, CHK computed : %d, raw_data : %d, temp = %d\n", val->chksum, chksum, raw_data, val->temp);
+}
+
+/**
+ * DHT22 init frame
+*/
+static void dht22_initialize(void){
+	count=0;
+	gpio_direction_output(gpio,1);
+	gpio_set_value(gpio,0);
+	udelay(Tbe);
+	enable_irq(irq);
+	gpio_set_value(gpio,1);
+	gpio_direction_input(gpio);
+
+	wait_for_completion_interruptible(&cpl);
+	disable_irq(irq);
+	gpio_direction_output(gpio,0);
+	data_management();
 }
 
 /**
  * Called on cat /sys/kernel/dht22_kobject/{temp,hum}
 */
 static ssize_t b_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-	gpio_direction_output(gpio,1);
-	//int var;
-	count=0;
-	gpio_set_value(gpio,0);
-	udelay(Tbe);
-	gpio_set_value(gpio,1);
-	gpio_direction_input(gpio);
-
-	wait_for_completion(&cpl);
-	printk(KERN_INFO "Buffer :%s , index :%d , count :%d\n",bit_buf,index,count);
-
-/*
+	dht22_initialize();
 	if (strcmp(attr->attr.name, "temp") == 0){
-		printk(KERN_INFO "Reading temperature\n");
 		var = temp;
 	}
 
 	else if(strcmp(attr->attr.name, "hum") == 0){
-		printk(KERN_INFO "Reading humidity\n");
 		var = hum;
 	}
 
@@ -123,9 +143,8 @@ static ssize_t b_show(struct kobject *kobj, struct kobj_attribute *attr, char *b
 		kobject_put(dht22_kobject);
 		return -EINVAL;
 	}
-*/
-//	return sprintf(buf, "%d\n", var);
-	return 0;
+	reinit_completion(&cpl);
+	return sprintf(buf, "%d\n", var);
 }
 
 /**
@@ -177,13 +196,15 @@ static int __init dht22_init(void){
 	gpio_direction_output(gpio,1);
 
 	init_completion(&cpl);
-
-	err = request_irq(gpio_to_irq(gpio),
+	irq = gpio_to_irq(gpio);
+	
+	err = request_irq(irq,
 			(irq_handler_t) dht22_irq_handler,
-			IRQF_TRIGGER_RISING|IRQF_TRIGGER_FALLING,
+			IRQF_TRIGGER_FALLING,
 			"dht22_gpio_handler",
 			NULL);
-	
+	disable_irq(irq);
+	val =vmalloc(sizeof(values));
 	return err;
 }
 
@@ -191,12 +212,11 @@ static int __init dht22_init(void){
  * Exitting LKM
 */
 static void __exit dht22_exit(void){
-	gpio_set_value(gpio,1);
 	gpio_unexport(gpio);
 	gpio_free(gpio);
 	kobject_put(dht22_kobject);
-	tasklet_kill(&dht22_tasklet);
-	free_irq(gpio_to_irq(gpio),NULL);
+	free_irq(irq,NULL);
+	vfree(val);
 	printk(KERN_INFO "Removing DHT22 module\n");
 }
 
